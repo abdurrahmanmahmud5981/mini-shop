6b9a40711a98d64c25808e90bc5c775d28540a1e
abdurrahmanmahmud5981 <ar7543078@gmail.com>
2025-07-08 13:34:54 +0600

    fix: remove unnecessary class from root div and ensure MainLayout has proper styling

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router'
 
 const MainLayout: React.FC = () => {
     return (
-        <div>
+        <div className='min-h-screen bg-gray-50'>
             <Navber />
             <main>
                 <Outlet />
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import MainLayout from '../layouts/MainLayout'
 import Home from '../pages/Home/Home'
+import { ROUTES } from '../constants/routes'
 const AppRouter: React.FC = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<MainLayout />}>
                     <Route index element={<Home />} />
+                    <Route path={ROUTES.PRODUCT} element={`Product Details`}/>
                 </Route>
             </Routes>
         </BrowserRouter>
